09c4b6329cd7d14d3eb49a1d0c8fb576adf2ac2b
MohammadHossein R Shojaodiny <83103488+Reynardd@users.noreply.github.com>
2023-05-17 07:58:00 +0330

    Bugs Fixed
    //

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-//test
+
 class container {
 
 	int size;
 public:
 	float* p;
 	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	int& getsize() { return size;}
 
 };
 
@@ -15,26 +15,32 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	 vector(int l) :len(l),container(1 * 100){
 		p = new float();
+		call_num = 0;
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen() {
+		call_num++;
 		return len;
 	}
+	vector(container c):container(1*100)
+	{
+		p = new float();
+		call_num = 0;
+	};
 	~vector() = default;
 };
 
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
+	vector v1(c1);
 	container& r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
 	vector v2 = 100;
-	v2.getlen = 40;
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
