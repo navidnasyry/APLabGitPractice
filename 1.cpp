c5068301edfd573e99fc6c0bf7498d085a4a7797
hsmmd <IhadimardanI@gmail.com>
2023-05-17 11:30:09 +0330

    1.cpp has changed

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
 using namespace std;
 
-class container {
+class container
+{
 
 	int size;
-public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
 
+public:
+	float *p;
+	container() {}
+	container(int s) : size(s) {}
+	const int &getsize() { return size; }
+
+	friend class vector;
 };
 
-class vector :public container {
-
-	int call_num;
-public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
-	}
+class vector : public container
+{
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int call_num;
+
+public:
+	vector() {}
+	explicit vector(int l)
+	{
+		p = new float();
+		len = l;
+		size = (1 * 100);
+	}
+	int &getlen()
+	{
 		return len;
 	}
 	~vector() = default;
 };
 
-int main() {
+int main()
+{
 
 	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
+	// vector v1 = c1; // you cant call parent in child
+	vector v1;
+	container &r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
+	// c2.getsize() = 20; u cant change the const size
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2 = (vector)100;
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
+
+// 10040
