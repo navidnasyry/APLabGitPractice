938f23e462c7872009cdf5c7cd38f715fb2517fe
AmirHami <amirthebest1383@gmail.com>
2023-05-17 12:19:45 +0330

    file 2changed

--- 2.cpp
+++ 2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 // count all the specific char in the whole array of strings
 int countAllSpecificChars(string sArr[], int arrLength, char specificChar) {
-    int count;
-    for (int i = 0; i <= arrLength; ++i)
-        for (int j = 0; j <= sArr[i].size(); ++j)
+    int count = 0;//the count variable must be initialized to 0
+    for (int i = 0; i < arrLength; ++i)//I changed the <= to < as because i range shuld be this
+        for (int j = 0; j < sArr[i].size(); ++j)//I changed the <= to < as it must be(j refers to index not number of element)
             // if the jth char of the string is the specific char
-            if (sArr[i][j] = specificChar)
+            if (sArr[i][j] == specificChar)//i changed the assainment operator to equality operator
                 count++;
     return count;
 }
