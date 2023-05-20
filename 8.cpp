559e4a9e3ac4ab992016b761a28c2b9127f5cf08
Mehran1383 <134122361+Mehran1383@users.noreply.github.com>
2023-05-20 20:23:35 +0330

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,4 +1,5 @@
-﻿#include<stdio.h>
+#include<stdio.h>
+
 const char * f(const char **p) {
 	auto q = (p + sizeof(char))[1];
 	return q;
@@ -7,7 +8,9 @@ int main() {
 	const char * str[] = { "Wish","You","Best",":D" };
 	printf("%c%c ", *f(str), *(f(str) + 1));
 	printf("%c%c%c%c\n", **str, *(*(str + 1) + 1), *((str + 2)[-1] + 1), **&*(&str[-1] + 1));
-
-
+	//the size of char is 1 so,
+	//the output is:
+	//Be WooW
+	
 	
 }
