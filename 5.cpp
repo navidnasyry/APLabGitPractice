8c9b2dec04f3b2bcc94c933cddabcb5a4b15c8b5
Mehran1383 <134122361+Mehran1383@users.noreply.github.com>
2023-05-20 19:49:01 +0330

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,4 +1,5 @@
-﻿#include<stdio.h>
+#include<stdio.h>
+
 int main()
 {
     int arr[] = { 10, 20, 30, 40, 50, 60 };
@@ -6,5 +7,8 @@ int main()
     int *ptr2 = arr + 5;
     printf("%d\n", (*ptr2 - *ptr1));
     printf("%c", (char)(*ptr2 - *ptr1));
+	// at first ptr1 points to the first house of arr so the first output is the answer of 60-10 that is 50!
+	// then we cast 50 to char that equals to '2'!
+	
     return 0;
 }
