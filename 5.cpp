5af6f901445a0a773c8df22a1749c130a3001689
unknown <naderi830602@gmail.com>
2023-05-21 00:19:34 -0700

    changed

--- 5.cpp
+++ 5.cpp
@@ -2,9 +2,10 @@
 int main()
 {
     int arr[] = { 10, 20, 30, 40, 50, 60 };
-    int *ptr1 = arr;
-    int *ptr2 = arr + 5;
+    int* ptr1 = arr;
+    int* ptr2 = arr + 5;
     printf("%d\n", (*ptr2 - *ptr1));
-    printf("%c", (char)(*ptr2 - *ptr1));
+    char s = (*ptr2 - *ptr1) + '0';
+    printf("%c", s);
     return 0;
 }
